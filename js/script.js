ca0d718a66a39f34565fd720d4168a7abd24a878
Val-Osad <v.p.osadchuk@gmail.com>
2022-06-08 13:42:36 +0000

    async func added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 //weather block
 const weatherBlock = document.querySelector('.wrapper');
+
+async function loadWeather(e) {
+    weatherBlock.innerHTML = `
+        <div class="weather-loading">
+            <img src="../img/loading.gif alt="loading...">
+        </div>`;
+}
+
+if(weatherBlock) {
+    loadWeather();
+}
